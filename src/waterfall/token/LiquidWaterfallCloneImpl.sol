5ab948916b5913d3fa15adc839483ea36598de97
samparsky <8148384+samparsky@users.noreply.github.com>
2023-05-23 17:37:00 +0300

    add: svg redenrer impl

--- src/waterfall/token/LiquidWaterfallCloneImpl.sol
+++ src/waterfall/token/LiquidWaterfallCloneImpl.sol
@@ -6,54 +6,57 @@ import {ERC1155} from "solmate/tokens/ERC1155.sol";
 import {LibString} from "solmate/utils/LibString.sol";
 import {utils} from "../../lib/Utils.sol";
 import {Base64} from "solady/utils/Base64.sol";
+import {Renderer} from "../../lib/Renderer.sol";
+
 
 error DoesNotExist();
 
 /// @notice Deposit contract wrapper which mints an NFT on successful deposit.
 /// @author Obol Labs Inc. (https://github.com/ObolNetwork)
 contract LiquidWaterfallCloneImpl is ERC1155, Ownable {
-  uint256 internal constant TOKEN_ID = 0;
 
-  function initialize(address[] calldata accounts) external {
-    // prevent from being initialized multiple times
-    require(owner() == address(0), "intialized");
+    uint256 internal constant TOKEN_ID = 0;
 
-    _initializeOwner(msg.sender);
+    function initialize(address[] calldata accounts) external {
+        // prevent from being initialized multiple times
+        require(owner() == address(0), "intialized");
+        
+        _initializeOwner(msg.sender);
 
-    uint256 numAccs = accounts.length;
-    unchecked {
-      for (uint256 i; i < numAccs; ++i) {
-        _mint({to: accounts[i], id: i, amount: 1, data: ""});
-      }
+        uint256 numAccs = accounts.length;
+        unchecked {
+            for (uint256 i; i < numAccs; ++i) {
+                _mint({to: accounts[i], id: i, amount: 1, data: ""});
+            }
+        }
     }
-  }
 
-  function uri(uint256 id) public view override returns (string memory) {
-    return string.concat(
-      "data:application/json;base64,",
-      Base64.encode(
-        bytes(
-          string.concat(
-            '{"name": "Liquid Split ',
-            utils.shortAddressToString(address(this)),
-            '", "description": ',
-            '"Each token represents 0.1% of this Liquid Split.", ',
-            '"external_url": ',
-            '"https://app.0xsplits.xyz/accounts/',
-            utils.addressToString(address(this)),
-            "/?chainId=",
-            utils.uint2str(block.chainid),
-            '", ',
-            '"image": "data:image/svg+xml;base64,',
-            Base64.encode(bytes(Renderer.render(address(this)))),
-            '"}'
-          )
-        )
-      )
-    );
-  }
+    function uri(uint256) public view override returns(string memory) {
+       return string.concat(
+            "data:application/json;base64,",
+            Base64.encode(
+                bytes(
+                    string.concat(
+                        '{"name": "Liquid Split ',
+                        utils.shortAddressToString(address(this)),
+                        '", "description": ',
+                        '"Each token represents 0.1% of this Liquid Split.", ',
+                        '"external_url": ',
+                        '"https://app.0xsplits.xyz/accounts/',
+                        utils.addressToString(address(this)),
+                        "/?chainId=",
+                        utils.uint2str(block.chainid),
+                        '", ',
+                        '"image": "data:image/svg+xml;base64,',
+                        Base64.encode(bytes(Renderer.render(address(this)))),
+                        '"}'
+                    )
+                )
+            )
+        );
+    }
 
-  function name() external view returns (string memory) {
-    return string.concat("Obol Liquid Waterfall Split ", utils.shortAddressToString(address(this)));
-  }
+    function name() external view returns (string memory) {
+        return string.concat("Obol Liquid Waterfall Split ", utils.shortAddressToString(address(this)));
+    }
 }
