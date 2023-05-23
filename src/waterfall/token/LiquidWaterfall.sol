bb904f09c6bc6a70328ead4f395f594fd54b2d8e
samparsky <8148384+samparsky@users.noreply.github.com>
2023-05-23 13:23:35 +0300

    edidting

--- src/waterfall/token/LiquidWaterfall.sol
+++ src/waterfall/token/LiquidWaterfall.sol
@@ -10,7 +10,7 @@ error DoesNotExist();
 
 /// @notice Deposit contract wrapper which mints an NFT on successful deposit.
 /// @author Obol Labs Inc. (https://github.com/ObolNetwork)
-contract LiquidWaterfall is ERC1155, Ownable {
+contract LiquidWaterfallCloneImpl is ERC1155, Ownable {
 
     uint256 internal constant TOKEN_ID = 0;
 
@@ -36,36 +36,3 @@ contract LiquidWaterfall is ERC1155, Ownable {
         return string.concat("Obol Liquid Waterfall Split ", utils.shortAddressToString(address(this)));
     }
 }
-
-// abstract contract LiquidWaterfallCloneImpl is Clone {
-
-//     /// @dev waterfall module
-//     IWaterfallFactoryModule public immutable waterfallFactoryModule;
-
-//     /// @dev liquid waterfall factory
-//     address internal immutable liquidWaterfallFactory;
-
-//     /// -----------------------------------------------------------------------
-//     /// constructor & initializer
-//     /// -----------------------------------------------------------------------
-
-//     constructor(address _waterfallModuleFactory) {
-//         waterfallFactoryModule = IWaterfallFactoryModule(_waterfallModuleFactory);
-//         liquidWaterfallFactory = msg.sender;
-//     }
-
-//     function initializer() internal {
-//         // waterfallFactoryModule.createWaterfallModule(token, nonWaterfallRecipient, recipients, thresholds);()
-//     }
-
-//     /// distributes ETH & ERC20s to NFT holders
-//     /// @param token ETH (0x0) or ERC20 token to distribute
-//     /// @param accounts Ordered, unique list of NFT holders
-//     /// @param distributorAddress Address to receive distributorFee
-//     function distributeFunds(address token, address[] calldata accounts, address distributorAddress) external virtual {
-        
-//     }
-
-
-//     // function create
-// }
