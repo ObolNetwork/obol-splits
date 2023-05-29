3f6167aeb28f32d8a48064ccaa48e555cfa38bb4
samparsky <8148384+samparsky@users.noreply.github.com>
2023-05-29 09:08:52 +0300

    add: test case

--- src/test/waterfall/LiquidWaterfallCloneImpl.t.sol
+++ src/test/waterfall/LiquidWaterfallCloneImpl.t.sol
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract LiquidWaterfallCloneImplTest is Test {
+import "splits-tests/Base.t.sol";
+import {LibCloneBase} from "splits-tests/LibClone.t.sol";
+
+
+contract LiquidWaterfallCloneImplTest is BaseTest, LibCloneBase  {
+    
     
 }
