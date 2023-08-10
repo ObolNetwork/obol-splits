b597da9fb03dc29457567d77cbfd24f0a3390a57
samparsky <8148384+samparsky@users.noreply.github.com>
2023-08-10 17:21:12 +0300

    chore: add owrfactory deployment script

---
+++ script/OWRFactory.s.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.17;
+
+import "forge-std/Script.sol";
+import {OptimisticWithdrawalRecipientFactory} from "src/waterfall/OptimisticWithdrawalRecipientFactory.sol";
+
+contract OWRFactoryScript is Script {
+  function run() external {
+    uint256 privKey = vm.envUint("PRIVATE_KEY");
+    vm.startBroadcast(privKey);
+    new OptimisticWithdrawalRecipientFactory{salt: keccak256("obol.owrFactory.v1")}();
+    vm.stopBroadcast();
+  }
+}
