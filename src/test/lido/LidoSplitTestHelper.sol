5c3f6d0c5191b8af4474b9adc47e572e666ec69a
samparsky <8148384+samparsky@users.noreply.github.com>
2023-08-14 12:30:57 +0300

    test: add test_CanDistribute test; add wstETH, stETH mainnet addresses

--- src/test/lido/LidoSplitTestHelper.sol
+++ src/test/lido/LidoSplitTestHelper.sol
@@ -3,6 +3,6 @@ pragma solidity 0.8.17;
 
 
 contract LidoSplitTestHelper {
-    address internal STETH_MAINNET_ADDRESS = address(0x1);
-    address internal WSTETH_MAINNET_ADDRESS = address(0x2);
+    address internal STETH_MAINNET_ADDRESS = address(0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84);
+    address internal WSTETH_MAINNET_ADDRESS = address(0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0);
 }
