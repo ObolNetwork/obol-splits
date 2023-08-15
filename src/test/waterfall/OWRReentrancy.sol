cf8f49c6c6cd9d9293fb4c0dfc8e508dcc967732
samparsky <8148384+samparsky@users.noreply.github.com>
2023-08-15 16:41:52 +0300

    chore: rename folder waterfall => owr

--- src/test/waterfall/OWRReentrancy.sol
+++ src/test/waterfall/OWRReentrancy.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.17;
 import "forge-std/Test.sol";
-import {OptimisticWithdrawalRecipient} from "src/waterfall/OptimisticWithdrawalRecipient.sol";
+import {OptimisticWithdrawalRecipient} from "src/owr/OptimisticWithdrawalRecipient.sol";
 
 contract OWRReentrancy is Test {
     receive() external payable {
