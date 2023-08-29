3d4fda96761d88d979f50ce937b9c49baaaea0a4
samparsky <8148384+samparsky@users.noreply.github.com>
2023-08-29 02:59:31 -0700

    feat: udpate imsc implementation and tests

--- src/test/controllers/IMSC.t.sol
+++ src/test/controllers/IMSC.t.sol
@@ -9,8 +9,10 @@ import {ISplitMain} from "src/interfaces/ISplitMain.sol";
 
 
 contract IMSC is Test {
+    error Initialized();
 
     address internal SPLIT_MAIN_GOERLI = 0x2ed6c4B5dA6378c7897AC67Ba9e43102Feb694EE;
+    uint256 public constant PERCENTAGE_SCALE = 1e6;
 
     ImmutableSplitControllerFactory public factory;
     ImmutableSplitController public cntrlImpl;
@@ -22,6 +24,8 @@ contract IMSC is Test {
 
     address[] controllerAccounts;
     uint32[] controllerPercentAllocations;
+
+    address split;
 
     function setUp() public {
         uint256 goerliBlock = 8_529_931;
@@ -41,7 +45,7 @@ contract IMSC is Test {
         controllerAccounts = new address[](3);
         controllerAccounts[0] = makeAddr("accounts0");
         controllerAccounts[1] = makeAddr("accounts1");
-        controllerAccounts[2] = makeAddr("accounts2");
+        controllerAccounts[2] = makeAddr("accounts3");
 
         controllerPercentAllocations = new uint32[](3);
         controllerPercentAllocations[0] = 400_000;
@@ -51,24 +55,198 @@ contract IMSC is Test {
         bytes32 deploymentSalt = keccak256(abi.encodePacked(uint256(64)));
         
         // predict controller address
-        address controller = factory.predictSplitControllerAddress(
+        address predictedControllerAddress = factory.predictSplitControllerAddress(
+            controllerAccounts,
+            controllerPercentAllocations,
+            0,
+            deploymentSalt
+        );
+
+        split = ISplitMain(SPLIT_MAIN_GOERLI).createSplit(
+            accounts,
+            percentAllocations,
+            0,
+            predictedControllerAddress
+        );
+
+        // deploy controller 
+        controller = factory.createController(
             split,
             controllerAccounts,
             controllerPercentAllocations,
             0,
-            keccak256(abi.encodePacked(uint256(64)))
+            deploymentSalt
+        );
+    }
+
+
+    function testCannot_DoubleInitialiseIMSC() public {
+        vm.expectRevert(
+            Initialized.selector
+        );
+        
+        controller.init(address(0x3));
+    }
+
+    function testCan_getSplitMain() public {
+        assertEq(controller.splitMain(), SPLIT_MAIN_GOERLI, "valid splitMain address");
+    }
+
+    function testCan_getDistributorFee() public {
+        assertEq(controller.distributorFee(), 0 , "invalid distributor fee");
+        
+        uint32 maxDistributorFee = 1e5;
+
+        ImmutableSplitController customController = factory.createController(
+            split,
+            controllerAccounts,
+            controllerPercentAllocations,
+            maxDistributorFee,
+            keccak256(abi.encodePacked(uint256(640)))
         );
 
-        address split = ISplitMain(SPLIT_MAIN_GOERLI).createSplit(
-            accounts,
-            percentAllocations,
+        assertEq(
+            customController.distributorFee(),
+            maxDistributorFee,
+            "invalid distributor fee"
+        );
+    }
+
+    function testCan_getSplitConfiguration() public {
+        (
+            address[] memory localAccounts,
+            uint32[] memory localPercentAllocations
+        ) = controller.getNewSplitConfiguration();
+
+        assertEq(
+            localAccounts, controllerAccounts, "invalid accounts"
+        ); 
+
+        assertEq(
+            localPercentAllocations.length,
+            controllerPercentAllocations.length,
+            "unequal length percent allocations"
+        );
+
+        for(uint i; i < localPercentAllocations.length; i++) {
+            assertEq(
+                uint256(localPercentAllocations[i]), 
+                uint256(controllerPercentAllocations[i]),
+                "invalid percentAllocations"
+            );
+        }
+    }
+
+    function testCan_getSplit() public {
+        assertEq(
+            controller.split(),
+            split
+        );
+    }
+
+    function testCan_updateSplit() public {
+        controller.updateSplit();
+
+        assertEq(
+            ISplitMain(SPLIT_MAIN_GOERLI).getHash(split),
+            _hashSplit(controllerAccounts, controllerPercentAllocations, 0),
+            "invalid split hash"
+        );
+    }
+
+    function testFuzz_updateSplit(
+        uint256 splitSeed,
+        uint256 controllerSeed,
+        uint8 splitSize,
+        uint8 controllerSize
+    ) public {
+        vm.assume(splitSeed != controllerSeed);
+        vm.assume(splitSize > 1);
+        vm.assume(controllerSize > 1);
+
+        address[] memory splitterAccts = _generateAddresses(splitSeed, splitSize);
+        address[] memory ctrllerAccounts = _generateAddresses(controllerSeed, controllerSize);
+
+        uint32[] memory splitterPercentAlloc = _generatePercentAlloc(splitSize);
+        uint32[] memory ctrllerPercentAlloc = _generatePercentAlloc(controllerSize);
+
+        bytes32 deploymentSalt = keccak256(abi.encodePacked(uint256(604)));
+
+        // predict controller address
+        address predictedControllerAddress = factory.predictSplitControllerAddress(
+            ctrllerAccounts,
+            ctrllerPercentAlloc,
             0,
-            controller
+            deploymentSalt
         );
 
-        // deploy controller 
+        // create split
+        address fuzzSplit = ISplitMain(SPLIT_MAIN_GOERLI).createSplit(
+            splitterAccts,
+            splitterPercentAlloc,
+            0,
+            predictedControllerAddress
+        );
 
+        // create controller
+        controller = factory.createController(
+            fuzzSplit,
+            ctrllerAccounts,
+            ctrllerPercentAlloc,
+            0,
+            deploymentSalt
+        );
 
+        // get current split hash
+        bytes32 currentSplitHash = ISplitMain(SPLIT_MAIN_GOERLI).getHash(fuzzSplit);
+        // update split
+        controller.updateSplit();
+
+        bytes32 newSplitHash = ISplitMain(SPLIT_MAIN_GOERLI).getHash(fuzzSplit);
+
+        bytes32 calculatedSplitHash = _hashSplit(
+            ctrllerAccounts,
+            ctrllerPercentAlloc,
+            0
+        );
+
+        assertTrue(currentSplitHash != newSplitHash, "update split hash");
+        assertEq(calculatedSplitHash, newSplitHash, "split hash equal");
+    }
+
+    function _hashSplit(
+        address[] memory accts,
+        uint32[] memory percentAlloc,
+        uint32 distributorFee
+    ) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked(accts, percentAlloc, distributorFee));
+    }
+
+    
+    function _generateAddresses(
+        uint256 _seed,
+        uint256 size
+    ) internal pure returns (address[] memory accts) {
+        accts = new address[](size);
+        uint160 seed = uint160(uint256(keccak256(abi.encodePacked(_seed))));
+        for(uint160 i ; i < size; i++) {
+            accts[i] = address(seed);
+            seed += 1;
+        }
+    }
+
+    function _generatePercentAlloc(
+        uint256 size
+    ) internal pure returns (uint32[] memory alloc) {
+
+        alloc = new uint32[](size);
+        for (uint i; i < size; i++) {
+            alloc[i] = uint32(PERCENTAGE_SCALE / size);
+        }
+
+        if (PERCENTAGE_SCALE % size != 0) {
+            alloc[size - 1] += uint32(PERCENTAGE_SCALE % size);
+        }
     }
 
 }
--- src/test/controllers/IMSCFactory.t.sol
+++ src/test/controllers/IMSCFactory.t.sol
@@ -10,7 +10,19 @@ import {ISplitMain} from "src/interfaces/ISplitMain.sol";
 
 contract IMSCFactory is Test {
 
+    error InvalidSplit__TooFewAccounts(uint256 accountsLength);
+    error InvalidSplit__AccountsAndAllocationsMismatch(
+        uint256 accountsLength,
+        uint256 allocationsLength
+    );
+    error InvalidSplit__InvalidAllocationsSum(uint32 allocationsSum);
+    error InvalidSplit__AccountsOutOfOrder(uint256 index);
+    error InvalidSplit__AllocationMustBePositive(uint256 index);
+    error InvalidSplit__InvalidDistributorFee(uint32 distributorFee);
+
+
     address internal SPLIT_MAIN_GOERLI = 0x2ed6c4B5dA6378c7897AC67Ba9e43102Feb694EE;
+    uint32 public constant SPLIT_MAIN_PERCENTAGE_SCALE = 1e6;
 
     ImmutableSplitControllerFactory public factory;
     ImmutableSplitController public cntrlImpl;
@@ -33,13 +45,145 @@ contract IMSCFactory is Test {
         percentAllocations[0] = 400_000;
         percentAllocations[1] = 600_000;
     }
+    
+    function test_RevertIfAccountSizeIsOne() public {
+        address[] memory newAccounts = new address[](1);
+        newAccounts[0] = makeAddr("testRevertIfAccountSizeIsOne");
 
-    function test_createController() public {
+        vm.expectRevert(
+            abi.encodeWithSelector(InvalidSplit__TooFewAccounts.selector, newAccounts.length)
+        );
+
+        factory.createController(
+            address(1),
+            newAccounts,
+            percentAllocations,
+            0,
+             keccak256(abi.encodePacked(uint256(12)))
+        );
+    }
+
+    function test_RevertIfAccountAndAllocationMismatch() public {
+        uint32[] memory newPercentAllocations = new uint32[](3);
+        newPercentAllocations[0] = 200_000;
+        newPercentAllocations[1] = 200_000;
+        newPercentAllocations[2] = 600_000;
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                InvalidSplit__AccountsAndAllocationsMismatch.selector,
+                accounts.length,
+                newPercentAllocations.length
+            )
+        );
+
+        factory.createController(
+            address(1),
+            accounts,
+            newPercentAllocations,
+            0,
+            keccak256(abi.encodePacked(uint256(12)))
+        );
+    }
+
+
+    function test_RevertIfAccountOutOfOrder() public {
+        address[] memory newAccounts = new address[](2);
+        newAccounts[0] = address(0x4);
+        newAccounts[1] = address(0x1);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                InvalidSplit__AccountsOutOfOrder.selector,
+                0
+            )
+        );
+
+        factory.createController(
+            address(1),
+            newAccounts,
+            percentAllocations,
+            0,
+            keccak256(abi.encodePacked(uint256(12)))
+        );
+    }
+
+    function test_RevertIfZeroPercentAllocation() public {
+        uint32[] memory newPercentAllocations = new uint32[](2);
+        newPercentAllocations[0] = SPLIT_MAIN_PERCENTAGE_SCALE;
+        newPercentAllocations[1] = 0;
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                InvalidSplit__AllocationMustBePositive.selector,
+                1
+            )
+        );
+
+        factory.createController(
+            address(1),
+            accounts,
+            newPercentAllocations,
+            0,
+            keccak256(abi.encodePacked(uint256(12)))
+        );
+    }
+
+    function test_RevertIfInvalidDistributorFee() public {
+        uint32 invalidDistributorFee = 1e6;
+        
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                InvalidSplit__InvalidDistributorFee.selector,
+                invalidDistributorFee
+            )
+        );
+
+        factory.createController(
+            address(1),
+            accounts,
+            percentAllocations,
+            invalidDistributorFee,
+            keccak256(abi.encodePacked(uint256(12)))
+        );
+    }
+
+    function test_RevertIfInvalidAllocationSum() public {
+        uint32[] memory newPercentAllocations = new uint32[](2);
+        newPercentAllocations[0] = SPLIT_MAIN_PERCENTAGE_SCALE;
+        newPercentAllocations[1] = 1;
+
+         vm.expectRevert(
+            abi.encodeWithSelector(
+                InvalidSplit__InvalidAllocationsSum.selector,
+                SPLIT_MAIN_PERCENTAGE_SCALE + 1
+            )
+        );
+
+        factory.createController(
+            address(1),
+            accounts,
+            newPercentAllocations,
+            0,
+            keccak256(abi.encodePacked(uint256(12)))
+        );
+    }
+
+    function test_CanCreateController() public {
+        bytes32 deploymentSalt =  keccak256(abi.encodePacked(uint256(1102)));
+
+        address predictedAddress = factory.predictSplitControllerAddress(
+            accounts,
+            percentAllocations,
+            0,
+            deploymentSalt
+        );
+
         address split = ISplitMain(SPLIT_MAIN_GOERLI).createSplit(
             accounts,
             percentAllocations,
             0,
-            address(0)
+            predictedAddress
         );
 
         ImmutableSplitController controller = factory.createController(
@@ -47,14 +191,7 @@ contract IMSCFactory is Test {
             accounts,
             percentAllocations,
             0,
-            keccak256(abi.encodePacked(uint256(12)))
-        );
-
-        address predictedAddress = factory.predictSplitControllerAddress(
-            accounts,
-            percentAllocations,
-            0,
-            keccak256(abi.encodePacked(uint256(12)))
+            deploymentSalt
         );
 
         assertEq(address(controller), predictedAddress, "predicted_address_invalid");
