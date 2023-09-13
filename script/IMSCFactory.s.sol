265e870b8efb20e75b2e75fcd5089dc1719e7bf0
samparsky <8148384+samparsky@users.noreply.github.com>
2023-09-13 13:31:40 +0100

    chore: add imscfactory deployment script

--- script/IMSCFactory.s.sol
+++ script/IMSCFactory.s.sol
@@ -5,11 +5,14 @@ import "forge-std/Script.sol";
 import {ImmutableSplitControllerFactory} from "src/controllers/ImmutableSplitControllerFactory.sol";
 
 contract IMSCFactoryScript is Script {
-  function run(address splitMain) external {
+  function run(address) external {
+    address SPLIT_MAIN_GOERLI = 0x2ed6c4B5dA6378c7897AC67Ba9e43102Feb694EE;
+
     uint256 privKey = vm.envUint("PRIVATE_KEY");
     vm.startBroadcast(privKey);
 
-    new ImmutableSplitControllerFactory{salt: keccak256("obol.imsc.v1")}(splitMain);
+    new ImmutableSplitControllerFactory{salt: keccak256("obol.imsc.v1")}(SPLIT_MAIN_GOERLI);
+
     vm.stopBroadcast();
   }
 }
