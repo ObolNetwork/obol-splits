0d0ac925ce3f85d0647aaf9f199d35a8b48645ed
samparsky <8148384+samparsky@users.noreply.github.com>
2023-09-13 12:49:07 +0100

    chore: add imsc deployment script

---
+++ script/IMSCFactory.s.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.17;
+
+import "forge-std/Script.sol";
+import {ImmutableSplitControllerFactory} from "src/controllers/ImmutableSplitControllerFactory.sol";
+
+contract IMSCFactoryScript is Script {
+  function run(address splitMain) external {
+    uint256 privKey = vm.envUint("PRIVATE_KEY");
+    vm.startBroadcast(privKey);
+
+    new ImmutableSplitControllerFactory{salt: keccak256("obol.imsc.v1")}(splitMain);
+    vm.stopBroadcast();
+  }
+}
