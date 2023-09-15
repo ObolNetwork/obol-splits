6575587b12284f0a22a177af394e5871108fb92f
samparsky <8148384+samparsky@users.noreply.github.com>
2023-09-15 12:17:52 +0100

    chore: upgrade solidity version 0.8.17 => 0.8.19

--- src/interfaces/IENSReverseRegistrar.sol
+++ src/interfaces/IENSReverseRegistrar.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.19;
 
 interface IENSReverseRegistrar {
   function claim(address owner) external returns (bytes32);
--- src/interfaces/ILiquidWaterfall.sol
+++ src/interfaces/ILiquidWaterfall.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity =0.8.17;
+pragma solidity ^0.8.19;
 
 interface ILiquidWaterfall {
   function balanceOf(address owner, uint256 id) external returns (uint256);
