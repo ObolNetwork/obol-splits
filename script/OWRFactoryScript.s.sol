d7b825b0dbe8e883d9b6fb4d698c0d691b0d15be
samparsky <8148384+samparsky@users.noreply.github.com>
2023-10-04 15:52:11 +0100

    test: fix OWRFactory test cases

--- script/OWRFactoryScript.s.sol
+++ script/OWRFactoryScript.s.sol
@@ -5,10 +5,22 @@ import "forge-std/Script.sol";
 import {OptimisticWithdrawalRecipientFactory} from "src/owr/OptimisticWithdrawalRecipientFactory.sol";
 
 contract OWRFactoryScript is Script {
-  function run() external {
+
+  function run(
+    string memory _name,
+    address _ensReverseRegistrar,
+    address _ensOwner
+  ) external {
     uint256 privKey = vm.envUint("PRIVATE_KEY");
+    
     vm.startBroadcast(privKey);
-    new OptimisticWithdrawalRecipientFactory{salt: keccak256("obol.owrFactory.v1")}();
+    
+    new OptimisticWithdrawalRecipientFactory{salt: keccak256("obol.owrFactory.v1")}(
+      _name,
+      _ensReverseRegistrar,
+      _ensOwner
+    );
+
     vm.stopBroadcast();
   }
 }
