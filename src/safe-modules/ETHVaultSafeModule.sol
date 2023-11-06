35d5ad8180b7136570598815e8b078fdc90220da
samparsky <8148384+samparsky@users.noreply.github.com>
2023-11-06 11:07:29 +0200

    feat: add eth vault gnosis safe module

---
+++ src/safe-modules/ETHVaultSafeModule.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.19;
+import {ERC20} from "solady/tokens/ERC20.sol";
+import {SafeTransferLib} from "solady/utils/SafeTransferLib.sol";
+import {IETH2DepositContract} from "../interfaces/IETH2DepositContract.sol";
+
+contract ETHVaultSafeModule is ERC20 {
+    using SafeTransferLib for address;
+
+    IETH2DepositContract immutable public depositContract;
+
+    constructor(address eth2DepositContract) {
+        depositContract = IETH2DepositContract(eth2DepositContract);
+    }
+
+    function name() public view override returns (string memory) {
+        // return 
+    }
+
+    function symbol() public view override returns (string memory) {
+
+    }
+
+    function setup() external {
+
+    }
+
+    function depositValidator(
+        bytes calldata pubkey,
+        bytes calldata withdrawal_credentials,
+        bytes calldata signature,
+        bytes32 deposit_data_root,
+        uint256 amount
+    ) external payable {
+        depositContract.deposit{value: amount}(
+            pubkey,
+            withdrawal_credentials,
+            signature,
+            deposit_data_root
+        );
+    }
+
+    function _deposit(address to, uint256 amount) internal {
+        _mint(to, amount);
+    }
+
+    function redeem(address to, uint256 amount) external {
+        _burn(msg.sender, amount);
+        to.safeTransferETH(amount);
+    }
+
+    receive() external payable {
+        _deposit(msg.sender, msg.value);
+    }
+
+}
