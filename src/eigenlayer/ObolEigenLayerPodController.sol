e90389e08569d8ebd0671b8102624dd5b2505d47
samparsky <8148384+samparsky@users.noreply.github.com>
2023-12-18 14:38:20 +0200

    feat: add eigenlayer implementation and test

---
+++ src/eigenlayer/ObolEigenLayerPodController.sol
@@ -0,0 +1,176 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.19;
+
+import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
+import {ERC20} from "solmate/tokens/ERC20.sol";
+import {Ownable} from "solady/auth/Ownable.sol";
+import {
+    IEigenLayerUtils,
+    IEigenPodManager,
+    IDelayedWithdrawalRouter
+} from "../interfaces/IEigenLayer.sol";
+
+
+/// @title ObolEigenLayerFactory
+/// @author Obol
+/// @notice A factory contract for cheaply deploying ObolLidoEigenLayer.
+/// @dev The address returned should be used to as reward address for Lido
+contract ObolEigenLayerPodController {
+
+    /// @dev returned on failed call
+    error CallFailed(bytes data);
+    /// @dev If Invalid fee setup
+    error Invalid_FeeSetup();
+    /// @dev user unauthorized
+    error Unauthorized();
+    /// @dev contract already initialized
+    error AlreadyInitialized();
+
+    /// @dev Emiited on intialize
+    event Initialized(address eigenPod, address owner);
+    
+    /// -----------------------------------------------------------------------
+    /// libraries
+    /// -----------------------------------------------------------------------
+    using SafeTransferLib for address;
+    using SafeTransferLib for ERC20;
+
+    uint256 internal constant PERCENTAGE_SCALE = 1e5;
+
+    /// -----------------------------------------------------------------------
+    /// storage - immutables
+    /// -----------------------------------------------------------------------
+
+    /// @notice address of Eigenlayer delegation manager
+    /// @dev This is the address of the delegation manager transparent proxy 
+    address public immutable eigenLayerDelegationManager;
+
+    /// @notice address of EigenLayerPod Manager
+    /// @dev this is the pod manager transparent proxy
+    IEigenPodManager public immutable eigenLayerPodManager;
+
+    /// @notice address of delay withdrawal router
+    IDelayedWithdrawalRouter public immutable delayedWithdrawalRouter;
+
+    /// @notice fee address
+    address public immutable feeRecipient;
+
+    /// @notice fee share
+    uint256 public immutable feeShare;
+
+    /// -----------------------------------------------------------------------
+    /// storage
+    /// -----------------------------------------------------------------------
+
+    /// @notice address of deployed Eigen pod
+    address public eigenPod;
+
+    /// @notice address of a splitter
+    address public split;
+
+    /// @notice address of owner
+    address public owner;
+
+
+    modifier onlyOwner() {
+        if (msg.sender != owner) {
+            revert Unauthorized();
+        }
+        _;
+    }
+
+    constructor(
+        address recipient,
+        uint256 share,
+        address delegationManager,
+        address eigenPodManager,
+        address withdrawalRouter
+    ) {
+        if (recipient != address(0) && share == 0) revert Invalid_FeeSetup();
+
+        feeRecipient = recipient;
+        feeShare = share;
+        eigenLayerDelegationManager = delegationManager;
+        eigenLayerPodManager = IEigenPodManager(eigenPodManager);
+        delayedWithdrawalRouter = IDelayedWithdrawalRouter(withdrawalRouter);
+    }
+
+    /// @dev Enables contract to receive ETH
+    // defined on the clone implementation
+    // receive() external payable {}
+
+    /// @notice initializes the controller
+    /// @param _owner address of the controller owner
+    /// @param splitter address of splitter
+    function initialize(address _owner, address splitter) external {
+        if (owner != address(0)) revert AlreadyInitialized();
+
+        eigenPod = eigenLayerPodManager.createPod();
+        owner = _owner;
+        split = splitter;
+
+        emit Initialized(eigenPod, _owner);
+    }
+
+    /// @notice Call the eigenPod contract
+    /// @param data to call eigenPod contract
+    function callEigenPod( 
+        bytes calldata data
+    ) external payable onlyOwner {
+        _executeCall(address(eigenPod), msg.value, data);
+    }
+
+    /// @notice Call the Eigenlayer delegation Manager contract
+    /// @param data to call eigenPod contract
+    function callDelegationManager(
+        bytes calldata data
+    ) external payable onlyOwner {
+        _executeCall(address(eigenLayerDelegationManager), msg.value, data);
+    }
+
+    /// @notice Call the Eigenlayer Manager contract
+    /// @param data to call contract
+    function callEigenPodManager(
+        bytes calldata data
+    ) external payable onlyOwner {
+        _executeCall(address(eigenLayerPodManager), msg.value, data);
+    }
+
+    /// @notice Withdraw funds from the delayed withdrawal router
+    /// @param numberOfDelayedWithdrawalsToClaim number of claims
+    function claimDelayedWithdrawals(uint256 numberOfDelayedWithdrawalsToClaim)
+        external
+    {
+        delayedWithdrawalRouter.claimDelayedWithdrawals(
+            address(this),
+            numberOfDelayedWithdrawalsToClaim
+        );
+
+        // transfer eth to split
+        uint256 balance = address(this).balance;
+        if (feeShare > 0) {
+            uint256 fee =  (balance * feeShare) / PERCENTAGE_SCALE;
+            feeRecipient.safeTransferETH(fee);
+            split.safeTransferETH(balance -= fee);
+        } else {
+            split.safeTransferETH(address(this).balance);
+        }
+    }
+
+    /// @notice Rescue stuck tokens
+    /// @return balance Amount of tokens rescued
+    function rescueFunds(address token) external returns (uint256 balance) {
+        balance = ERC20(token).balanceOf(address(this));
+        if (balance > 0) ERC20(token).safeTransfer(split, balance);
+    }
+
+    /// @notice Execute a low level call
+    /// @param to address to execute call
+    /// @param value amount of ETH to send with call
+    /// @param data bytes array to execute
+    function _executeCall(address to, uint256 value, bytes memory data) internal {
+        (bool success,) = address(to).call{value: value}(data);
+        if (!success) revert CallFailed(data);
+    }
+
+}
---
+++ src/test/eigenlayer/ObolEigenLayerPodController.t.sol
@@ -0,0 +1,197 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.19;
+import "forge-std/Test.sol";
+import { ObolEigenLayerPodController } from "src/eigenlayer/ObolEigenLayerPodController.sol";
+import { ObolEigenLayerPodControllerFactory } from "src/eigenlayer/ObolEigenLayerPodControllerFactory.sol";
+import { IEigenPod, IDelegationManager, IEigenPodManager, IEigenLayerUtils, IDelayedWithdrawalRouter } from "src/interfaces/IEigenLayer.sol";
+import {MockERC20} from "src/test/utils/mocks/MockERC20.sol";
+
+interface IDepositContract {
+
+    function deposit(
+        bytes calldata pubkey,
+        bytes calldata withdrawal_credentials,
+        bytes calldata signature,
+        bytes32 deposit_data_root
+    ) external payable;
+
+}
+contract ObolEigenLayerPodControllerTest is Test {
+    error Unauthorized();
+    error NewOwnerIsZeroAddress();
+    error NoHandoverRequest();
+
+    address constant DEPOSIT_CONTRACT_GOERLI = 0xff50ed3d0ec03aC01D4C79aAd74928BFF48a7b2b;
+    address constant DELEGATION_MANAGER_GOERLI = 0x1b7b8F6b258f95Cf9596EabB9aa18B62940Eb0a8;
+    address constant POD_MANAGER_GOERLI = 0xa286b84C96aF280a49Fe1F40B9627C2A2827df41;
+    address constant DELAY_ROUTER_GOERLI = 0x89581561f1F98584F88b0d57c2180fb89225388f;
+
+    address constant EIGEN_LAYER_OPERATOR_GOERLI = 0x3DeD1CB5E25FE3eC9811B918A809A371A4965A5D;
+
+    ObolEigenLayerPodControllerFactory factory;
+    ObolEigenLayerPodController controller;
+    address owner;
+    address user1;
+    address user2;
+    address splitter;
+    address feeRecipient;
+
+    uint256 feeShare;
+
+    MockERC20 mERC20;
+
+    function setUp() public {
+        uint256 goerliBlock = 10_205_449;
+        vm.createSelectFork(getChain("goerli").rpcUrl, goerliBlock);
+
+        vm.mockCall(
+            DEPOSIT_CONTRACT_GOERLI, abi.encodeWithSelector(IDepositContract.deposit.selector), bytes.concat(bytes32(0))
+        );
+
+        owner = makeAddr("owner");
+        user1 = makeAddr("user1");
+        user1 = makeAddr("user2");
+        splitter = makeAddr("splitter");
+        feeRecipient = makeAddr("feeRecipient");
+        feeShare = 1e3;
+
+        factory = new ObolEigenLayerPodControllerFactory(
+            feeRecipient,
+            feeShare,
+            DELEGATION_MANAGER_GOERLI,
+            POD_MANAGER_GOERLI,
+            DELAY_ROUTER_GOERLI
+        );
+
+        controller = ObolEigenLayerPodController(factory.createPodController(
+            owner,
+            splitter
+        ));
+        
+        mERC20 = new MockERC20("Test Token", "TOK", 18);
+        mERC20.mint(type(uint256).max);
+
+    }
+
+    function test_RevertIfNotOwnerCallEigenPod() external {
+        vm.prank(user1);
+        vm.expectRevert(Unauthorized.selector);
+        controller.callEigenPod(
+            encodeEigenPodCall(user1, 1 ether)
+        );
+    }
+
+    function test_CallEigenPod() external {
+        address pod = controller.eigenPod();
+        uint256 amount = 1 ether;
+
+        // airdrop ether to pod
+        (bool success,) = pod.call{value: amount}("");
+        require(success, "call failed");
+
+        vm.prank(owner);
+        controller.callEigenPod(
+            encodeEigenPodCall(user1, amount)
+        );
+    }
+
+    function test_CallDelegationManager() external {
+        vm.prank(owner);
+        controller.callDelegationManager(
+            encodeDelegationManagerCall(
+                EIGEN_LAYER_OPERATOR_GOERLI
+            )
+        );
+    }
+
+    function test_OnlyOwnerCallDelegationManager() external {
+        vm.prank(user1);
+        vm.expectRevert(Unauthorized.selector);
+        controller.callDelegationManager(
+            encodeDelegationManagerCall(EIGEN_LAYER_OPERATOR_GOERLI)
+        );
+    }
+
+    function test_CallEigenPodManager() external {
+        uint256 etherStake = 32 ether;
+        vm.deal(owner, etherStake + 1 ether);
+        vm.prank(owner);
+        controller.callEigenPodManager{value: etherStake}(
+            encodeEigenPodManagerCall(0)
+        );
+    }
+
+    function test_OnlyOwnerEigenPodManager() external {
+        vm.expectRevert(Unauthorized.selector);
+        controller.callEigenPodManager(
+            encodeEigenPodManagerCall(0)
+        );
+    }
+
+
+    function test_ClaimDelayedWithdrawals() external {
+        address DELAY_ROUTER_OWNER = 0x37bAFb55BC02056c5fD891DFa503ee84a97d89bF ;
+        vm.prank(DELAY_ROUTER_OWNER);
+        // set the delay withdrawal duration to zero
+        IDelayedWithdrawalRouter(DELAY_ROUTER_GOERLI).setWithdrawalDelayBlocks(0);
+
+        uint256 amountToDeposit = 2 ether;
+
+        // transfer unstake beacon eth to eigenPod
+        (bool success, ) = address(controller.eigenPod()).call{value: amountToDeposit}("");
+        require(success, "call failed");
+
+        vm.startPrank(owner);
+        {
+            controller.callEigenPod(
+                encodeEigenPodCall(address(controller), amountToDeposit)
+            );
+            controller.claimDelayedWithdrawals(1);
+        }
+        vm.stopPrank();
+
+        assertEq(
+            address(feeRecipient).balance,
+            20000000000000000,
+            "user balance increased"
+        );
+        assertEq(
+            address(splitter).balance,
+            1980000000000000000,
+            "user balance increased"
+        );
+    }
+
+    function test_RescueFunds() external {
+        uint256 amount = 1e18;
+        mERC20.transfer(address(controller), amount);
+        
+        controller.rescueFunds(address(mERC20));
+
+        assertEq(
+            mERC20.balanceOf(splitter),
+            amount,
+            "could not rescue funds"
+        );
+    }
+
+    function encodeEigenPodCall(address recipient, uint256 amount) internal pure returns (bytes memory callData) {
+        callData = abi.encodeCall(IEigenPod.withdrawNonBeaconChainETHBalanceWei, (recipient, amount));
+    }
+
+    function encodeDelegationManagerCall(address operator) internal pure returns (bytes memory callData) {
+        IEigenLayerUtils.SignatureWithExpiry memory signature = IEigenLayerUtils.SignatureWithExpiry(
+            bytes(""), 0
+        );
+        callData = abi.encodeCall(IDelegationManager.delegateTo, (operator, signature, bytes32(0)));
+    }
+
+    function encodeEigenPodManagerCall(uint256) internal pure returns (bytes memory callData) {
+        bytes memory pubkey = bytes("");
+        bytes memory signature = bytes("");
+        bytes32 dataRoot = bytes32(0);
+
+        callData = abi.encodeCall(IEigenPodManager.stake, (pubkey, signature, dataRoot));
+    }
+
+}
