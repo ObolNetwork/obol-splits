31d0f44667bea589e4d7d91b1ae2adebaaa5798f
cosminobol <cosmin@obol.tech>
2024-02-29 17:38:35 +0200

    chore: ran formatter on Base contracts

--- src/base/BaseSplit.sol
+++ src/base/BaseSplit.sol
@@ -6,109 +6,98 @@ import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
 import {Clone} from "solady/utils/Clone.sol";
 
 abstract contract BaseSplit is Clone {
-    error Invalid_Address();
-    error Invalid_FeeShare();
+  error Invalid_Address();
+  error Invalid_FeeShare();
 
-    /// -----------------------------------------------------------------------
-    /// libraries
-    /// -----------------------------------------------------------------------
-    using SafeTransferLib for ERC20;
-    using SafeTransferLib for address;
+  /// -----------------------------------------------------------------------
+  /// libraries
+  /// -----------------------------------------------------------------------
+  using SafeTransferLib for ERC20;
+  using SafeTransferLib for address;
 
-    address internal constant ETH_ADDRESS = address(0);
-    uint256 internal constant PERCENTAGE_SCALE = 1e5;
+  address internal constant ETH_ADDRESS = address(0);
+  uint256 internal constant PERCENTAGE_SCALE = 1e5;
 
-    /// @notice fee share
-    uint256 public immutable feeShare;
+  /// @notice fee share
+  uint256 public immutable feeShare;
 
-    /// @notice fee address
-    address public immutable feeRecipient;
+  /// @notice fee address
+  address public immutable feeRecipient;
 
-    // withdrawal (adress, 20 bytes)
-    // 0; first item
-    uint256 internal constant WITHDRAWAL_ADDRESS_OFFSET = 0;
-    // 20 = withdrawalAddress_offset (0) + withdrawalAddress_size (address, 20 bytes)
-    uint256 internal constant TOKEN_ADDRESS_OFFSET = 20;
+  // withdrawal (adress, 20 bytes)
+  // 0; first item
+  uint256 internal constant WITHDRAWAL_ADDRESS_OFFSET = 0;
+  // 20 = withdrawalAddress_offset (0) + withdrawalAddress_size (address, 20 bytes)
+  uint256 internal constant TOKEN_ADDRESS_OFFSET = 20;
 
-    constructor(address _feeRecipient, uint256 _feeShare) {
-        if (_feeShare == 0 || _feeShare > PERCENTAGE_SCALE) revert Invalid_FeeShare();
+  constructor(address _feeRecipient, uint256 _feeShare) {
+    if (_feeShare == 0 || _feeShare > PERCENTAGE_SCALE) revert Invalid_FeeShare();
 
-        feeShare = _feeShare;
-        feeRecipient = _feeRecipient;
+    feeShare = _feeShare;
+    feeRecipient = _feeRecipient;
+  }
+
+  /// -----------------------------------------------------------------------
+  /// View
+  /// -----------------------------------------------------------------------
+
+  /// Address to send funds to to
+  /// @dev equivalent to address public immutable withdrawalAddress
+  function withdrawalAddress() public pure returns (address) {
+    return _getArgAddress(WITHDRAWAL_ADDRESS_OFFSET);
+  }
+
+  /// Token addresss
+  /// @dev equivalent to address public immutable token
+  function token() public pure virtual returns (address) {
+    return _getArgAddress(TOKEN_ADDRESS_OFFSET);
+  }
+
+  /// -----------------------------------------------------------------------
+  /// Public
+  /// -----------------------------------------------------------------------
+
+  /// @notice Rescue stuck ETH and tokens
+  /// Uses token == address(0) to represent ETH
+  /// @return balance Amount of ETH or tokens rescued
+  function rescueFunds(address tokenAddress) external virtual returns (uint256 balance) {
+    _beforeRescueFunds(tokenAddress);
+
+    if (tokenAddress == ETH_ADDRESS) {
+      balance = address(this).balance;
+      if (balance > 0) withdrawalAddress().safeTransferETH(balance);
+    } else {
+      balance = ERC20(tokenAddress).balanceOf(address(this));
+      if (balance > 0) ERC20(tokenAddress).safeTransfer(withdrawalAddress(), balance);
     }
+  }
 
-    /// -----------------------------------------------------------------------
-    /// View
-    /// -----------------------------------------------------------------------
+  /// @notice distribute funds to withdrawal address
+  function distribute() external virtual {
+    uint256 amount = 0;
+    address tokenAddress = token();
 
-    /// Address to send funds to to
-    /// @dev equivalent to address public immutable withdrawalAddress
-    function withdrawalAddress() public pure returns (address) {
-        return _getArgAddress(WITHDRAWAL_ADDRESS_OFFSET);
+    if (tokenAddress == ETH_ADDRESS) amount = address(this).balance;
+    else amount = ERC20(tokenAddress).balanceOf(address(this));
+
+    if (feeShare > 0) {
+      //TODO check if feeShares > 0
+      uint256 fee = (amount * feeShare) / PERCENTAGE_SCALE;
+      _transfer(tokenAddress, feeRecipient, fee);
+      _transfer(tokenAddress, withdrawalAddress(), amount -= fee);
+    } else {
+      _transfer(tokenAddress, withdrawalAddress(), amount);
     }
+  }
 
-    /// Token addresss
-    /// @dev equivalent to address public immutable token
-    function token() public pure virtual returns (address) {
-        return _getArgAddress(TOKEN_ADDRESS_OFFSET);
-    }
+  /// -----------------------------------------------------------------------
+  /// Internal
+  /// -----------------------------------------------------------------------
 
-    /// -----------------------------------------------------------------------
-    /// Public
-    /// -----------------------------------------------------------------------
+  function _beforeRescueFunds(address tokenAddress) internal virtual {}
 
-    /// @notice Rescue stuck ETH and tokens
-    /// Uses token == address(0) to represent ETH
-    /// @return balance Amount of ETH or tokens rescued
-    function rescueFunds(address tokenAddress) external virtual returns (uint256 balance) {
-        _beforeRescueFunds(tokenAddress);
-
-        if (tokenAddress == ETH_ADDRESS) {
-            balance = address(this).balance;
-            if (balance > 0) withdrawalAddress().safeTransferETH(balance);
-        } else {
-            balance = ERC20(tokenAddress).balanceOf(address(this));
-            if (balance > 0) ERC20(tokenAddress).safeTransfer(withdrawalAddress(), balance);
-        }
-    }
-
-     /// @notice distribute funds to withdrawal address
-    function distribute() external virtual {
-        uint256 amount = 0;
-        address tokenAddress = token();
-
-        if (tokenAddress == ETH_ADDRESS) {
-            amount = address(this).balance;
-        } else {
-            amount = ERC20(tokenAddress).balanceOf(address(this));
-        }
-
-        if (feeShare > 0) {
-            //TODO check if feeShares > 0
-            uint256 fee = (amount * feeShare) / PERCENTAGE_SCALE;
-            _transfer(tokenAddress, feeRecipient, fee);
-            _transfer(tokenAddress, withdrawalAddress(), amount -= fee);
-        } else {
-            _transfer(tokenAddress, withdrawalAddress(), amount);
-        }
-        
-    }
-
-    /// -----------------------------------------------------------------------
-    /// Internal
-    /// -----------------------------------------------------------------------
-
-    function _beforeRescueFunds(address tokenAddress) internal virtual {}
-
-    function _transfer(
-        address tokenAddress, 
-        address receiver,
-        uint256 amount
-    ) internal {
-        if (tokenAddress == ETH_ADDRESS) {
-            receiver.safeTransferETH(amount);
-        } else {
-            ERC20(tokenAddress).safeTransfer(receiver, amount);
-        }
-    }
+  function _transfer(address tokenAddress, address receiver, uint256 amount) internal {
+    if (tokenAddress == ETH_ADDRESS) receiver.safeTransferETH(amount);
+    else ERC20(tokenAddress).safeTransfer(receiver, amount);
+  }
 }
