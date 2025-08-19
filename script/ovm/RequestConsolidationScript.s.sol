ec640290a3106854e25e5f06787059c15a1c96e3
Andrei Smirnov <andrei@obol.tech>
2025-08-19 17:32:38 +0300

    Better OVM scripts and readme

---
+++ script/ovm/RequestConsolidationScript.s.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "forge-std/Script.sol";
+import "./Utils.s.sol";
+import {ObolValidatorManager} from "src/ovm/ObolValidatorManager.sol";
+
+//
+// This script calls requestConsolidation() for a ObolValidatorManager contract.
+// To run this script, the following environment variables must be set:
+// - PRIVATE_KEY: the private key of the account that will deploy the contract
+//
+contract RequestConsolidationScript is Script {
+  function run(address ovmAddress, bytes calldata src, bytes calldata dst) external {
+    uint256 privKey = vm.envUint("PRIVATE_KEY");
+    if (privKey == 0) {
+      revert("set PRIVATE_KEY env var before using this script");
+    }
+    if (!Utils.isContract(ovmAddress)) {
+      revert("Invalid OVM address");
+    }
+
+    vm.startBroadcast(privKey);
+
+    ObolValidatorManager ovm = ObolValidatorManager(payable(ovmAddress));
+
+    bytes[] memory sourcePubKeys = new bytes[](1);
+    sourcePubKeys[0] = src;
+
+    ovm.requestConsolidation{value: 100 wei}(sourcePubKeys, dst);
+
+    vm.stopBroadcast();
+  }
+}
